5d4d312f80081ad00af606d69730700b8700b52a
Prakhar Pundir <prakharpundir@Prakhars-MacBook-Air-2.local>
2026-03-09 00:37:06 +0530

    Buffer audio chunks in worklet to reduce websocket message frequency

--- public/audio-processor.worklet.js
+++ public/audio-processor.worklet.js
@@ -1,10 +1,24 @@
 // public/audio-processor.worklet.js
 class PCMProcessor extends AudioWorkletProcessor {
+    constructor() {
+        super();
+        this.bufferSize = 2048; // 128ms at 16000Hz (approx 7.8 chunks per second)
+        this.buffer = new Float32Array(this.bufferSize);
+        this.bufferIndex = 0;
+    }
+
     process(inputs) {
         const input = inputs[0];
-        if (input && input[0]) {
-            // Send raw Float32 audio data to the main thread
-            this.port.postMessage(input[0]);
+        if (!input || !input[0]) return true;
+
+        const channelData = input[0];
+        for (let i = 0; i < channelData.length; i++) {
+            this.buffer[this.bufferIndex++] = channelData[i];
+            if (this.bufferIndex >= this.bufferSize) {
+                // Send a copy to the main thread
+                this.port.postMessage(this.buffer.slice());
+                this.bufferIndex = 0;
+            }
         }
         return true; // Keep processor alive
     }
